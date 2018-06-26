4dd3c5c6ba8595f05d368ae2826d39434cb0dc09
Nolan Neeley <nolanneeley@gmail.com>
2018-06-26 16:39:36 -0500

    opposite number kata

---
+++ opposite_number.rb
@@ -0,0 +1,14 @@
+# Source: https://www.codewars.com/kata/opposite-number
+#
+# Instructions:
+# Given a number, find its opposite.
+# Examples:
+# 1: -1
+# 14: -14
+# -34: 34
+
+def opposite(x)
+ puts -(x)
+end
+
+opposite(5)
