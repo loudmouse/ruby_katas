55afdc4b716146ae642723c4dbd48654d64d6a70
Nolan Neeley <nolanneeley@gmail.com>
2018-06-28 17:18:31 -0500

    how many vowels kata

---
+++ how_many_vowels.rb
@@ -0,0 +1,18 @@
+# Source: https://edabit.com/challenge/WsxbmH9myo9SqT4sw
+#
+# Instructions:
+# Create a function that takes a string and returns
+# the number (count) of vowels contained within it.
+
+def count_vowels(str)
+  vowels_found = []
+  letters = str.downcase.split("")
+  letters.each do |x|
+    if x == "a" || x == "e" || x == "i" || x == "o" || x == "u"
+      vowels_found << x
+    end
+  end
+  p vowels_found.count
+end
+
+count_vowels("Intelligence")
