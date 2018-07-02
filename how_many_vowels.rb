1401f32feb1e929254c1b57443bb0d240cbea5be
Nolan Neeley <nolanneeley@gmail.com>
2018-07-02 15:11:01 -0500

    refactored with count method

--- how_many_vowels.rb
+++ how_many_vowels.rb
@@ -4,15 +4,20 @@
 # Create a function that takes a string and returns
 # the number (count) of vowels contained within it.
 
+# def count_vowels(str)
+#   vowels_found = []
+#   letters = str.downcase.split("")
+#   letters.each do |x|
+#     if x == "a" || x == "e" || x == "i" || x == "o" || x == "u"
+#       vowels_found << x
+#     end
+#   end
+#   p vowels_found.count
+# end
+
+#refactored
 def count_vowels(str)
-  vowels_found = []
-  letters = str.downcase.split("")
-  letters.each do |x|
-    if x == "a" || x == "e" || x == "i" || x == "o" || x == "u"
-      vowels_found << x
-    end
-  end
-  p vowels_found.count
+  p vowels = (str.count("aeiou") + str.count("AEIOU"))
 end
 
 count_vowels("Intelligence")
